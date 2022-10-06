35348e5723a935d81f06058f4cd8c34305f589d5
RX <70211426+roroloveorea@users.noreply.github.com>
2022-10-06 11:02:45 +0800

    Delete run_build.sh

--- run_build.sh
+++
@@ -1,9 +0,0 @@
-WORKSPACE=/home/yusuff/Desktop/Image_Class
-
-docker run -it --rm \
-	--gpus all \
-	--net host \
-	--shm-size 8G \
-    -w $WORKSPACE \
-	-v $WORKSPACE:$WORKSPACE \
-	tensorflow_docker
